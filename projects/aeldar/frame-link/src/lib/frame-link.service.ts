38305faf37a56e6995d7007591ab8d8e7626ea47
Eldar Aliyev <eldar.aliyev8@gmail.com>
2018-07-31 00:15:29 +0300

    add skeleton api

--- projects/aeldar/frame-link/src/lib/frame-link.service.ts
+++ projects/aeldar/frame-link/src/lib/frame-link.service.ts
@@ -1,4 +1,11 @@
 import { Injectable, Optional } from '@angular/core';
+import { Observable } from 'rxjs';
+
+interface Link {
+  tell: (channel: string) => <T>(what: T) => void;
+  ask: (channel: string) => <T, U>(what: T) => Observable<U>;
+  listen: (channel: string) => <T>(what: T) => void;
+}
 
 @Injectable()
 export class FrameLinkServiceConfig {
@@ -17,5 +24,13 @@ export class FrameLinkService {
     } else {
       throw new Error('FrameLinkService: appId was not provided! Please see documentation on using FrameLinkService.');
     }
+  }
+
+  linkTo(id: string): Link {
+    return {
+      tell: (channel) => (what) => { /* TODO */ },
+      ask: (channel) => (what) => { /* TODO */ return null; },
+      listen: (channel) => (what) => { /* TODO */ return null; },
+    };
   }
 }
